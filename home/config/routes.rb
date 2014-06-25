11644438a3b2948adb29d1ff8d58456c589ae255
tdouce <travisdouce@gmail.com>
2014-06-25 17:42:25 -0400

    Add and configure RemoteFactoryGirlHomeRails

--- home/config/routes.rb
+++ home/config/routes.rb
@@ -3,6 +3,10 @@ Home::Application.routes.draw do
 
   resources :schools
 
+  if defined?(RemoteFactoryGirlHomeRails::Engine)
+    mount RemoteFactoryGirlHomeRails::Engine, at: '/remote_factory_girl'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
