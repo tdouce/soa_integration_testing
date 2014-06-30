648a3c3b64253e2ae64cd1d93969c161fc109c35
tdouce <travisdouce@gmail.com>
2014-06-30 14:00:15 -0400

    Add comments in remote_factory_girl_spec
    Help clarify why the test works

--- client/spec/remote_factory_girl_spec.rb
+++ client/spec/remote_factory_girl_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
+# See spec/spec_helper.rb for configuring RemoteFactoryGirl (i.e. client) to 
+# coordinate with RemoteFactoryGirlHomeRails (i.e. home)
+
 describe 'working with RemoteFactoryGirl' do
   describe '#create' do
+
+    # Requires that a factory be defined in 'home' with name 'user'
     it 'should be able to create a user' do
       user = RemoteFactoryGirl.create(:user)
 
       expect(user).to have_key('first_name')
     end
 
+    # Requires that a factory be defined in 'home' with name 'user' and have #first_name
+    # and #last_name attributes
     it 'should be able to create a user with custom attributes' do
       user = RemoteFactoryGirl.create(:user, first_name: 'Sam', last_name: 'Iam')
 
