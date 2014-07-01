d3737ad449f45bafbc62ef8835e3a6c49f4e3263
tdouce <travisdouce@gmail.com>
2014-07-01 09:33:51 -0400

    Adds comment describing purpose of 'authenticate'

--- home/app/controllers/application_controller.rb
+++ home/app/controllers/application_controller.rb
@@ -7,7 +7,17 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # An authentication method that you might want to skip while running tests in 'client'
+  # An authentication method that exists only to demonstrate that methods can be
+  # skipped by configuring RemoteFactoryGirlHomeRails and RemoteDatabaseCleanerHomeRails
+  #   - https://github.com/tdouce/soa_integration_testing/blob/master/home/config/initializers/remote_factory_girl_home_rails.rb
+  #   - https://github.com/tdouce/soa_integration_testing/blob/master/home/config/initializers/remote_database_cleaner_home_rails.rb
+  #
+  # `unless params[:just_retrieving_resources_to_prove_they_exist_in_test]`
+  # exists only to allow the 'client' to fetch resources from 'home' to prove that
+  # resources are being created in 'home' from the 'client'
+  #   - https://github.com/tdouce/soa_integration_testing/blob/master/client/spec/spec_helper.rb#L59
+  #   - https://github.com/tdouce/soa_integration_testing/blob/master/client/spec/remote_factory_girl_spec.rb#L13
+  #
   def authenticate
     raise 'You are not authenticated!' unless params[:just_retrieving_users_to_prove_they_exist_in_test]
   end
