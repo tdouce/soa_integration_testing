dedcad5522d6b122b8ab4c9f71fc1ba2fee33955
tdouce <travisdouce@gmail.com>
2014-10-13 09:44:19 -0400

    Adds context blocks to remote_factory_girl_spec in client app

--- client/spec/remote_factory_girl_spec.rb
+++ client/spec/remote_factory_girl_spec.rb
@@ -8,48 +8,61 @@ require 'spec_helper'
 #
 describe 'working with RemoteFactoryGirl' do
   describe '#create' do
-    it 'should be able to create a user' do
-      user           = RemoteFactoryGirl.with_remote(:home_1).create(:user)
-      user_from_home = fetch_user_by_id_from_home(user['id'])
+    context 'with remote "home_1"' do
+      it 'should be able to create a user' do
+        user           = RemoteFactoryGirl.with_remote(:home_1).create(:user)
+        user_from_home = fetch_user_by_id_from_home(user['id'])
 
-      expect(user['first_name']).to eq(user_from_home['first_name']) 
-    end
-
-    it 'should be able to create a user with custom attributes' do
-      user           = RemoteFactoryGirl.with_remote(:home_1).create(:user, first_name: 'Sam')
-      user_from_home = fetch_user_by_id_from_home(user['id'])
-
-      expect(user['first_name']).to eq(user_from_home['first_name']) 
-    end
-
-    describe 'associations' do
-      it 'should be able to create associations using factories' do
-        school_with_principal = RemoteFactoryGirl.with_remote(:home_1).create(:school_with_principal, name: 'Tift County High School')
-        school_from_home      = fetch_school_by_id_from_home(school_with_principal['id'])
-        user_from_home        = fetch_user_by_id_from_home(school_with_principal['id'])
-
-        expect(school_with_principal['name']).to eq(school_from_home['name'])
-        expect(school_with_principal['id']).to eq(user_from_home['id'])
+        expect(user['first_name']).to eq(user_from_home['first_name'])
       end
 
-      it 'should be able to manually create an assocation between a user and a school' do
-        school           = RemoteFactoryGirl.with_remote(:home_1).create(:school, name: 'Tift County High School')
-        user             = RemoteFactoryGirl.with_remote(:home_1).create(:user, first_name: 'Sam', last_name: 'Iam', school_id: school.fetch('id'))
-        school_from_home = fetch_school_by_id_from_home(school['id'])
-        user_from_home   = fetch_user_by_id_from_home(user['id'])
+      it 'should be able to create a user with custom attributes' do
+        user           = RemoteFactoryGirl.with_remote(:home_1).create(:user, first_name: 'Sam')
+        user_from_home = fetch_user_by_id_from_home(user['id'])
 
-        expect(school['name']).to eq(school_from_home['name'])
         expect(user['first_name']).to eq(user_from_home['first_name'])
-        expect(user['school_id']).to eq(school['id'])
+      end
+
+      describe 'associations' do
+        it 'should be able to create associations using factories' do
+          school_with_principal = RemoteFactoryGirl.with_remote(:home_1).create(:school_with_principal, name: 'Tift County High School')
+          school_from_home      = fetch_school_by_id_from_home(school_with_principal['id'])
+          user_from_home        = fetch_user_by_id_from_home(school_with_principal['id'])
+
+          expect(school_with_principal['name']).to eq(school_from_home['name'])
+          expect(school_with_principal['id']).to eq(user_from_home['id'])
+        end
+
+        it 'should be able to manually create an assocation between a user and a school' do
+          school           = RemoteFactoryGirl.with_remote(:home_1).create(:school, name: 'Tift County High School')
+          user             = RemoteFactoryGirl.with_remote(:home_1).create(:user, first_name: 'Sam', last_name: 'Iam', school_id: school.fetch('id'))
+          school_from_home = fetch_school_by_id_from_home(school['id'])
+          user_from_home   = fetch_user_by_id_from_home(user['id'])
+
+          expect(school['name']).to eq(school_from_home['name'])
+          expect(user['first_name']).to eq(user_from_home['first_name'])
+          expect(user['school_id']).to eq(school['id'])
+        end
+      end
+    end
+
+
+      end
+
+
       end
     end
   end
 
   describe '.factories' do
-    it 'should be able to retrieve a list of available factories' do
-      available_factories = RemoteFactoryGirl.with_remote(:home_1).factories
+    context 'with remote "home_1"' do
+      it 'should be able to retrieve a list of available factories' do
+        available_factories = RemoteFactoryGirl.with_remote(:home_1).factories
 
-      expect(available_factories['factories']).to match_array(['school_with_principal', 'user', 'school'])
+        expect(available_factories['factories']).to match_array(['school_with_principal', 'user', 'school'])
+      end
+    end
+
     end
   end
 end
