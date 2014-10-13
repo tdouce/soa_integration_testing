9cb55809a3bb9fc73554cab9dba3d27322fa679b
tdouce <travisdouce@gmail.com>
2014-10-13 10:29:33 -0400

    Update bootstrap.sh
    * Provide instructions to set up applications home_1, home_2, and
    client application
    * Provide instructions to run client applications test suite

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -16,6 +16,19 @@ bundle exec rake db:setup
 let success=$?+success
 
 echo ""
+echo "preparing 'home_2' application"
+echo ""
+cd ../home_2/
+bundle
+let success=success+$?
+bundle exec rake db:drop:all
+let success=$?+success
+RAILS_ENV=test bundle exec rake db:setup
+let success=$?+success
+bundle exec rake db:setup
+let success=$?+success
+
+echo ""
 echo "preparing 'client' application"
 echo ""
 cd ../client/
@@ -23,19 +36,20 @@ bundle
 
 cd ../
 
-HOME=$PWD/home
+HOME_1=$PWD/home
+HOME_2=$PWD/home_2
 CLIENT=$PWD/client
 
 function run_tests {
-  echo "1. cd to 'home' directory by executing:"
-  echo "   $ cd $HOME"
-  echo "2. Start 'home' application's server on port '3000' in 'test' environment by executing:"
-  echo "   $ bundle exec rails server --environment=test --pid=$HOME/tmp/pids/home-test.pid --port=3000"
-  echo "3. Open a new tab and cd to the 'client' by executing:"
-  echo "   $ cd $CLIENT"
-  echo "4. In the 'client' directory, run the 'client' test suite by executing:"
-  echo "   $ bundle exec rspec"
-
+  echo "1. Open a new tab from the command line."
+  echo "2. In that tab, cd to 'home' application and start 'home' application's server on port '3000' in 'test' environment by executing:"
+  echo "   $ cd $HOME_1 && bundle exec rails server --environment=test --pid=$HOME_1/tmp/pids/home-test.pid --port=3000"
+  echo "3. Open a new tab from the command line."
+  echo "4. In that tab, cd to 'home_2' directory and start 'home_2' application's server on port '4000' in 'test' environment by executing:"
+  echo "   $ cd $HOME_2 && bundle exec rails server --environment=test --pid=$HOME_2/tmp/pids/home-test.pid --port=4000"
+  echo "5. Open a new tab from the command line."
+  echo "6. In that tab, cd to the 'client' application and run the 'client' application's test suite by executing:"
+  echo "   $ cd $CLIENT && bundle exec rspec"
 }
 
 printf "\e[36m"
